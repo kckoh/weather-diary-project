9d411196149556d90a5b15e953aa0422a8d1b176
kckoh <kyungcheolkoh@gmail.com>
2022-09-11 22:23:47 -0700

    api documentaiton implemented

--- src/main/java/zerobase/weather/controller/DiaryController.java
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -1,5 +1,8 @@
 package zerobase.weather.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
@@ -15,27 +18,32 @@ public class DiaryController {
     private final DiaryService diaryService;
 
     @PostMapping("/create/diary")
-    void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, @RequestBody String text) {
+    @ApiOperation(value = "Using diary texts and weather, it saves to DB", notes = "")
+    void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "Weather Format: yyyy-MM-dd") LocalDate date, @RequestBody String text) {
         diaryService.createDiary(date, text);
     }
 
     @GetMapping("/read/diary")
-    List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+    @ApiOperation(value = "Fetch the selected date diary")
+    List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "Weather Format: yyyy-MM-dd")LocalDate date) {
         return diaryService.readDiary(date);
     }
 
     @GetMapping("/read/diaries")
-    List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+    @ApiOperation(value = "Fetch selected diatires given the dates")
+    List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "Weather Format: yyyy-MM-dd")LocalDate startDate, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "Weather Format: yyyy-MM-dd")LocalDate endDate) {
         return diaryService.readDiaries(startDate, endDate);
     }
 
     @PutMapping("/update/diary")
+    @ApiOperation(value = "Update a diary given the date", notes = "If they are multiple, fetch only one and update")
     void updateDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, @RequestBody String text) {
         diaryService.updateDiary(date, text);
     }
 
     @DeleteMapping("/delete/diary")
-    void deleteDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+    @ApiOperation(value = "Delete a diary given the date")
+    void deleteDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "Weather Format: yyyy-MM-dd")LocalDate date) {
         diaryService.deleteDiary(date);
     }
 }
